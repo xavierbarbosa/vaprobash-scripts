704197285b79ad28efc1e0de323f18462b7e0671
Xavier Barbosa <xavierbarbosa@gmail.com>
2014-05-05 16:38:03 +0100

    Update remove-host.sh

--- remove-host.sh
+++ remove-host.sh
@@ -6,4 +6,4 @@ sudo rm /etc/apache2/sites-available/000-default.conf
 sudo rm /etc/apache2/sites-enabled/000-default.conf
 
 # Restart Apache
-sudo service apache restart
+sudo service apache2 restart
