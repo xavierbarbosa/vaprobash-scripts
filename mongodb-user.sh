89eaa09363d20615e1d1a59705b77fcf815068cf
Xavier <xavier@madewithlove.be>
2014-05-06 02:59:14 +0100

    Adds a user to a mongodb database.

---
+++ mongodb-user.sh
@@ -0,0 +1,6 @@
+# Adds user to a mongodb database
+# ////////////////////////////////////////////////////////////////////
+
+echo 'db.addUser("$2", "$3", true);' > mongo.js
+mongo $1 mongo.js
+sudo rm mongo.js
